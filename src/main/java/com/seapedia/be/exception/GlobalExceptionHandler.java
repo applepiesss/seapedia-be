ea9a875cfa3aef1e7ef42a38442decb67de9d6bc
applepiesss <nadiaaiiisyahf@gmail.com>
2026-07-05 04:19:16 +0700

    Added error handling

---
+++ src/main/java/com/seapedia/be/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.seapedia.be.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleIllegalArgument(IllegalArgumentException exception) {
+        return Map.of("message", exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidation(MethodArgumentNotValidException exception) {
+        String message = exception.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .findFirst()
+                .map(error -> error.getField() + " " + error.getDefaultMessage())
+                .orElse("Validation failed");
+
+        return Map.of("message", message);
+    }
+}
