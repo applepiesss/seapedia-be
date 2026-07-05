12e0c46be7fb27fbb282567119215a77dd8dcbdb
applepiesss <nadiaaiiisyahf@gmail.com>
2026-07-05 17:11:31 +0700

    Added buyer cart models

---
+++ src/main/java/com/seapedia/be/model/Cart.java
@@ -0,0 +1,23 @@
+package com.seapedia.be.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "carts")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class Cart {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "buyer_id", nullable = false, unique = true)
+    private User buyer;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<CartItem> items = new ArrayList<>();
+}
